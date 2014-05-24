ff7ea1b55337973f11a9117bfbbf51e72eecd0b7
jvidalg <jesus.vidal.go@gmail.com>
2014-05-23 22:43:03 -0500

    Created new File scriptExample

---
+++ scriptExample.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while [ true ]
+do
+	IFS=$'\n'       # make newlines the only separator
+	set -f          # disable globbing
+	for i in $(cat customcrontab); do
+		startDate=${i%%||*}
+		task=${i##*||}
+		periodTask=${i#*||}
+		period=${periodTask%||*}
+
+		startSecs=`date -d "$startDate" +%s`
+		nowSecs=`date +%s`
+
+		# Check if the start date has reached
+		if (( $startSecs > $nowSecs )); then
+			continue
+		fi
+
+		let modulus=($nowSecs-$startSecs)%$period
+		if [ $modulus -eq 0 ]; then
+			eval "$task &"
+		fi
+	done
+	sleep 1
+done
+
